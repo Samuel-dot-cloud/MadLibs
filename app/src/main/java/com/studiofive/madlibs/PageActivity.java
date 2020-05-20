aaa1f2d4b3a903c17c62c20b9509e2d832ee8ecb
Samuel-dot-cloud <successam336@gmail.com>
2020-05-20 10:34:00 +0300

    Initial commit

---
+++ app/src/main/java/com/studiofive/madlibs/PageActivity.java
@@ -0,0 +1,13 @@
+package com.studiofive.madlibs;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page);
+    }
+}
